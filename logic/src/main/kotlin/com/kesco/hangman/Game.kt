d28f74bd6c99064c90e23c6679a9d0694a85c8a9
林进森 <kesco@Kesco-Macbook.local>
2016-01-18 22:10:27 +0800

    Added game function tests and try to pass them.

---
+++ logic/src/main/kotlin/com/kesco/hangman/Game.kt
@@ -0,0 +1,30 @@
+package com.kesco.hangman
+
+/**
+ * [HangMan](http://www.twoplayergames.org/play/668-Hangman.html) Logic
+ */
+class Game() {
+    fun new() {
+        /* Empty */
+    }
+
+    fun input(c: Char): Boolean {
+        return false
+    }
+
+    fun length(): Int {
+        return 5
+    }
+
+    fun tries(): Int {
+        return 11
+    }
+
+    fun used(): String {
+        return "a"
+    }
+
+    fun answer(): String {
+        return "kesco"
+    }
+}
--- logic/src/test/kotlin/com/kesco/hangman/GameTest.kt
+++ logic/src/test/kotlin/com/kesco/hangman/GameTest.kt
@@ -1,3 +1,26 @@
 package com.kesco.hangman
 
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
+class GameTest() {
+    var game: Game? = null
+
+    @Before
+    fun setUp() {
+        game = Game()
+    }
+
+    @Test
+    fun testoperationTest() {
+        game?.new()
+        assertFalse(game!!.input('a'))
+        assertEquals(5, game!!.length())
+        assertEquals(11, game!!.tries())
+        assertEquals("a", game!!.used())
+        assertEquals("kesco", game!!.answer())
+    }
+}
